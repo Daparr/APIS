5115943283e81c4539205a8bf2c2e2c444dd9948
Daparr <50182871+Daparr@users.noreply.github.com>
2021-12-28 19:07:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+from typing import Type
+from flask import Flask, request, json, session, Response
+from dbservice import Database, DBError
+from flask_cors import CORS
+
+def response_f(response, status):
+	return Response(
+			response=json.dumps(response),
+			status=status,
+			mimetype='application/json'
+			)
+
+class ApiErrors:
+    def __init__(self) -> None:
+        pass
+    @staticmethod
+    def DataError(args):
+        return 'Data not provided' + " ".join(args)
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/items/new', methods=['POST'])
+def new_item():
+    json_body = request.get_json()
+    try:
+        name = json_body['name']
+        content = json_body['content']
+        autor = json_body['autor']
+    except KeyError:
+        return response_f({'Message':ApiErrors.DataError(['name', 'content'])},400)
+    try:
+        Database.new_item(Database(),name,content,autor)
+    except DBError as e:
+        return response_f({'Message':e}, 400)
+    return response_f({'Message': 'Success'}, 200)
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    try:
+        items = Database.get_items(Database())
+    except DBError as e:
+        return response_f({'Message':e}, 400)
+    return response_f(items, 200)
+
+@app.route('/item/<id>', methods=['GET'])
+def get_item(id):
+    try:
+        item = Database.get_item(Database(),id)
+    except DBError as e:
+        return response_f({'Message':e}, 400)
+    return response_f(item, 200)
+
+@app.route('/item/<id>', methods=['DELETE'])
+def del_item(id):
+    try:
+        Database.del_item(Database(),id)
+    except DBError as e:
+        return response_f({'Message':e}, 400)
+    return response_f({'Message': 'Success'}, 200)
+
+@app.route('/item/<id>', methods=['PUT'])
+def update_item(id):
+    json_body = request.get_json()
+    try:
+        name = json_body['name']
+        content = json_body['content']
+        autor = json_body['autor']
+    except KeyError:
+        return response_f({'Message':ApiErrors.DataError(['name', 'content'])},400)
+    try:
+        Database.update_item(Database(),id,name,content,autor)
+    except DBError as e:
+        return response_f({'Message':e}, 400)
+    return response_f({'Message': 'Success'}, 200)
