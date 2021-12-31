bcb3b8d0ff0d60995854e7f87e7c1e4bf8d24e11
vr242kj <88940299+vr242kj@users.noreply.github.com>
2021-12-31 15:17:55 +0100

    Update article.ts

--- client/src/types/article.ts
+++ client/src/types/article.ts
@@ -1,6 +1,6 @@
 //import * as mongoose from 'mongoose';
 //export let ObjectId = mongoose.Schema.Types.ObjectId;
-import { Schema, Types } from 'mongoose';
+
 export class Article {
   _id: string
   name: string
